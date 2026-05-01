35088cd910ed3262d65ca8c7cfa3f6c1f7e9108a
Sleddie <JDelmuhametov@gmail.com>
2026-05-01 14:44:53 +0300

    Add project files.

---
+++ DefaultSolutions/FileExplore.cs
@@ -0,0 +1,55 @@
+﻿namespace DefaultSolutions
+{
+    public static class FileExplore
+    {
+        public static string OpenAndReadText(string? sourceFilePath)
+        {
+            if (string.IsNullOrWhiteSpace(sourceFilePath)
+                || !File.Exists(sourceFilePath))
+            {
+                return "";
+            }
+
+            string text;
+
+            try
+            {
+                text = File.ReadAllText(sourceFilePath);
+            }
+            catch
+            {
+                string fileName = Path.GetFileName(sourceFilePath);
+                string openPath =
+                    Path.Combine(AppContext.BaseDirectory, fileName);
+
+                try
+                {
+                    File.Copy(sourceFilePath, openPath, true);
+                    text = File.ReadAllText(openPath);
+                }
+                catch
+                {
+                    return "";
+                }
+
+                try
+                {
+                    File.Delete(openPath);
+                }
+                finally { }
+            }
+
+            return text;
+        }
+
+        public static string OpenAndReadText(this FileInfo sourceFile)
+        {
+            if (sourceFile is null)
+            {
+                return "";
+            }
+
+            return OpenAndReadText(sourceFile.FullName);
+        }
+    }
+}
