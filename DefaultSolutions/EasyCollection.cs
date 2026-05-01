8390830fc0b1b3f52c71d66025810c3ccc17331a
Sleddie <JDelmuhametov@gmail.com>
2026-05-01 22:21:50 +0300

    Dictionary extension

---
+++ DefaultSolutions/EasyCollection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+
+namespace DefaultSolutions
+{
+    public static class EasyCollection
+    {
+        public static TValue? TryGet<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary,
+            TKey? key)
+        {
+            if (key is null or "")
+            {
+                return default;
+            }
+
+            dictionary.TryGetValue(
+                key,
+                out TValue? serverInfo);
+            return serverInfo;
+        }
+
+        public static TValue? TryGet<TKey, TValue>(
+            this Hashtable hashtable,
+            TKey? key)
+        {
+            if (key is null or "")
+            {
+                return default;
+            }
+
+            if (!hashtable.ContainsKey(key))
+            {
+                return default;
+            }
+
+            object? objectValue = hashtable[key];
+            return (TValue?)objectValue;
+        }
+    }
+}
